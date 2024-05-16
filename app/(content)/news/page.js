ce0e111e58e0f9ff20119ded0cf675d101e4309b
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-05-16 09:48:07 +0500

    Fetching data with fecth api like in react

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,46 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
+
 import NewsList from "@/components/news-list";
+import { useEffect, useState } from "react";
 
 const NewsPage = () => {
+  const [news, setNews] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchNews() {
+      setLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setError("Faild to fetch news.");
+        setLoading(false);
+      }
+
+      const news = await response.json();
+      setNews(news);
+      setLoading(false);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (loading) {
+    return <p>Loading....</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 };
